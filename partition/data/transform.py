a6239ac3a55f998587a40e14207ed9531de53dd7
Ne0 <chenxi1228@foxmail.com>
2023-08-09 01:37:52 +0800

    add argparse

--- partition/data/transform.py
+++ partition/data/transform.py
@@ -1,23 +1,38 @@
 import pandas as pd
- 
-df = pd.read_csv("./ml_taobao.csv")
-df = df.drop(['Unnamed: 0','label','idx'],axis=1)
-df.to_csv("./taobao.csv",index=False)
+import argparse
 
-fr = open('./taobao.csv', 'rt')
-fw = open('./taobao_new.txt', 'w+') 
-  
-ls = []
- 
-for line in fr:
-    
-    line = line.replace('\n', '')
-    line = line.split(',')  
-    ls.append(line)  
 
-for row in ls:
-    fw.write(' '.join(row) + '\n')
-  
-  
-fr.close() 
-fw.close() 
+def csv2txt(data):
+    df = pd.read_csv(f'./data/ml_{data}.csv')
+    df = df.drop(['Unnamed: 0', 'label', 'idx'], axis=1)
+    df.to_csv(f'./partition/data/{data}.csv', index=False, header=False)
+
+    fr = open(f'./partition/data/{data}.csv', 'rt')
+    fw = open(f'./partition/data/{data}.txt', 'w+')
+
+    ls = []
+
+    for line in fr:
+        line = line.replace('\n', '')
+        line = line.split(',')
+        ls.append(line)
+
+    for row in ls:
+        fw.write(' '.join(row) + '\n')
+
+    fr.close()
+    fw.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default='wikipedia', help='dataset name')
+    # parser.add_argument('--csv_file', type=str, default='./partition/data/ml_wikipedia.csv', help='csv file to trans')
+    # parser.add_argument('--txt_file', type=str, default='./partition/data/wikipedia.txt', help='name and save the txt_file')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    csv2txt(args.data)
